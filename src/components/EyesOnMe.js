f637d7a4d75387baac294b8be87c07bfe269edd1
Dwire <gjdwyer8@gmail.com>
2018-05-16 19:58:15 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+  render(){
+    const someFocus = () => console.log('Good!');
+    const someBlur = () => console.log('Hey! Eyes on me!');
+
+    return (
+      <button onFocus={ someFocus } onBlur={ someBlur }>Button</button>
+    )
+  }
+}
+
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component {
+  render() {
+    const someShit = () => {console.log('Entering password...')}
+
+    return (
+      <input type={"password"} onKeyUp={someShit}></input>
+    )
+  }
+}
+
+export default Keypad
